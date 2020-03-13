ebe32e38f636258eba3bc342560a07795f863a93
Kizito Njoku <nkizito57@gmail.com>
2020-03-13 01:59:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code here
 function saturdayFun (day= "bathe my dog") {
-   return `This Saturday, I want to ${day}!`;
+   console.log(`This Saturday, I want to ${day}!`);
 }
 saturdayFun("roller-skate")
