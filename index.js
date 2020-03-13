289e481011679e4d0d276bb9b6cedd7ac6a73fd8
Kizito Njoku <nkizito57@gmail.com>
2020-03-13 02:05:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun (day= "roller-skate") {
+   return `This Saturday, I want to ${day}!`;
+}
+saturdayFun("bathe my dog");
+
+ function mondayWork (arg= "go to the office")  {
+  return `This Monday, I will ${arg}.`  
+}
+
+mondayWork("work from home")
+
+function wrapAdjective () {}
